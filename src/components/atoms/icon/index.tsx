6b47c7a7e497267fdd630af7bf11be4b3140a61a
hrahul2605 <hrahul2605@gmail.com>
2020-12-15 12:00:33 +0530

    -add: icons

--- src/components/atoms/icon/index.tsx
+++ src/components/atoms/icon/index.tsx
@@ -3,9 +3,23 @@ import { tuple } from '../../utils';
 
 // Assets
 import { ReactComponent as ForwardIcon } from './assets/forward.svg';
+import { ReactComponent as ConfigureIcon } from './assets/configure.svg';
+import { ReactComponent as BackIcon } from './assets/back.svg';
+import { ReactComponent as CloseIcon } from './assets/close.svg';
+import { ReactComponent as LoadingIcon } from './assets/loading.svg';
+import { ReactComponent as MinusIcon } from './assets/minus.svg';
+import { ReactComponent as PlusIcon } from './assets/plus.svg';
 
 // Icon Types
-const IconTypes = tuple('forward');
+const IconTypes = tuple(
+  'forward',
+  'configure',
+  'back',
+  'close',
+  'loading',
+  'minus',
+  'plus',
+);
 export type IconType = typeof IconTypes[number];
 
 interface Props extends React.SVGProps<SVGSVGElement> {
@@ -18,7 +32,18 @@ const Icon: React.FC<Props> = (props): JSX.Element => {
   switch (type) {
     case 'forward':
       return <ForwardIcon {...rest} className={className} />;
-
+    case 'configure':
+      return <ConfigureIcon {...rest} className={className} />;
+    case 'back':
+      return <BackIcon {...rest} className={className} />;
+    case 'close':
+      return <CloseIcon {...rest} className={className} />;
+    case 'loading':
+      return <LoadingIcon {...rest} className={className} />;
+    case 'minus':
+      return <MinusIcon {...rest} className={className} />;
+    case 'plus':
+      return <PlusIcon {...rest} className={className} />;
     default:
       return <></>;
   }
