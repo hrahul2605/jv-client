2ca67f68d4d59f96b34d01903c2d2ba53a1e1b6a
hrahul2605 <hrahul2605@gmail.com>
2021-03-20 08:21:53 +0530

    fixed input focus on label click, autoFocus prop

--- src/components/atoms/input/index.tsx
+++ src/components/atoms/input/index.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import classnames from 'classnames';
 import { tuple } from '../../utils';
 import './styles.css';
@@ -17,30 +17,50 @@ interface Props {
   size?: SizeType;
   label: string;
   caption?: string;
+  className?: string;
+  autoFocus?: boolean;
 }
 
-const Input: React.FC<Props> = (props): JSX.Element => {
-  const { label, size, type, caption } = props;
+const Input: React.FC<Props> = (props): React.ReactElement => {
+  const { label, size, type, caption, className, autoFocus } = props;
   const classProps = classnames({ [`input-${size}`]: true }, 'input-base');
+  const standardContainerClass = classnames(className, 'input-container');
+  const areaContainerClass = classnames(className, 'input-textarea-container');
+
+  const inputRef = useRef<HTMLInputElement>(null);
+  const textAreaRef = useRef<HTMLTextAreaElement>(null);
+
+  useEffect(() => {
+    if (autoFocus) {
+      if (type === 'standard') inputRef.current?.focus();
+      else textAreaRef.current?.focus();
+    }
+  }, []);
+
   switch (type) {
     case 'standard':
       return (
         <>
-          <div className="input-container">
-            <input required className={classProps} />
+          <div className={standardContainerClass}>
+            <input ref={inputRef} required className={classProps} />
             <label className="input-text">{label}</label>
           </div>
-          {caption ? <span className="input-caption">{caption}</span> : null}
+          {caption && <span className="input-caption">{caption}</span>}
         </>
       );
     case 'area':
       return (
         <>
-          <div className="input-textarea-container">
-            <textarea maxLength={60} required className="input-textarea" />
+          <div className={areaContainerClass}>
+            <textarea
+              ref={textAreaRef}
+              maxLength={60}
+              required
+              className="input-textarea"
+            />
             <label className="input-textarea-label">{label}</label>
           </div>
-          {caption ? <span className="input-caption">{caption}</span> : null}
+          {caption && <span className="input-caption">{caption}</span>}
         </>
       );
     default:
