62641bafd0d9f75c45382b8f0f07ae9c00f20845
hrahul2605 <hrahul2605@gmail.com>
2021-03-20 11:45:21 +0530

    added new routes for all pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { Landing } from './components/pages';
+import { Nav } from './components/organisms';
+import ErrorBoundary from './ErrorBoundary';
+import { Text } from './components/atoms';
 
-const App: React.FC = (): JSX.Element => {
+const Landing = lazy(() => import('./components/pages/landing'));
+const Create = lazy(() => import('./components/pages/create'));
+const Login = lazy(() => import('./components/pages/login'));
+
+const App: React.FC = (): React.ReactElement => {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={Landing} />
-      </Switch>
-    </BrowserRouter>
+    <ErrorBoundary>
+      <BrowserRouter>
+        <Nav />
+        <Suspense fallback={<Text>Loading</Text>}>
+          <Switch>
+            <Route exact path="/" component={Landing} />
+            <Route path="/login" component={Login} />
+            <Route path="/create" component={Create} />
+          </Switch>
+        </Suspense>
+      </BrowserRouter>
+    </ErrorBoundary>
   );
 };
 
