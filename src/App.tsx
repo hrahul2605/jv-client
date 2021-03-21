77191ac880c798e0950988f71960ca3f55cd07e7
hrahul2605 <hrahul2605@gmail.com>
2021-03-21 07:54:11 +0530

    update app with authenticated route

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,30 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Nav } from './components/organisms';
 import ErrorBoundary from './ErrorBoundary';
 import { Text } from './components/atoms';
+import { useTypedSelector } from './reducers';
 
 const Landing = lazy(() => import('./components/pages/landing'));
 const Create = lazy(() => import('./components/pages/create'));
 const Login = lazy(() => import('./components/pages/login'));
 
 const App: React.FC = (): React.ReactElement => {
+  const { authenticated } = useTypedSelector(state => state.global);
+
   return (
     <ErrorBoundary>
       <BrowserRouter>
         <Nav />
-        <Suspense fallback={<Text>Loading</Text>}>
+        <Suspense
+          fallback={
+            <div className="h-screen flex flex-1 bg-background">
+              <Text>Loading</Text>
+            </div>
+          }
+        >
           <Switch>
             <Route exact path="/" component={Landing} />
             <Route path="/login" component={Login} />
-            <Route path="/create" component={Create} />
+            {authenticated && <Route path="/create" component={Create} />}
           </Switch>
         </Suspense>
       </BrowserRouter>
