76ca43777483cd00d4b9d243fd56f07562aacf02
hrahul2605 <hrahul2605@gmail.com>
2021-03-21 07:48:47 +0530

    rival input molecule to forward ref, added onAdd, onRemove props

--- src/components/molecules/rivalInput/index.tsx
+++ src/components/molecules/rivalInput/index.tsx
@@ -3,27 +3,49 @@ import { Button, Icon, Input } from '../../atoms';
 
 interface Props {
   type: 'add' | 'remove';
+  name?: string;
+  defaultValue?: string;
+  ref?:
+    | React.RefObject<HTMLInputElement>
+    // eslint-disable-next-line no-unused-vars
+    | ((instance: HTMLInputElement | null) => void)
+    | null;
+  onAdd?: () => void;
+  onRemove?: () => void;
 }
 
-const RivalInput: React.FC<Props> = (props): React.ReactElement => {
-  const { type } = props;
-  return (
-    <div className="flex flex-row mb-2 relative items-center">
-      <Input label="Add Rival" />
-      <Button
-        size="s-icon"
-        theme="subtle"
-        className="absolute -right-12"
-        icon={
-          <Icon
-            type={type === 'add' ? 'plus' : 'minus'}
-            className={type === 'add' ? 'text-success' : 'text-error'}
-            width={10}
-          />
-        }
-      />
-    </div>
-  );
-};
+const RivalInput: React.FC<Props> = React.forwardRef(
+  (props, ref): React.ReactElement => {
+    const { type, onAdd, onRemove, name, defaultValue } = props;
+    const onClick = () => {
+      if (type === 'add' && onAdd) onAdd();
+      else if (type === 'remove' && onRemove) onRemove();
+    };
+    return (
+      <div className="flex flex-row mb-2 relative items-center">
+        <Input
+          label="Add Rival"
+          ref={ref}
+          name={name}
+          autoFocus
+          defaultValue={defaultValue}
+        />
+        <Button
+          size="s-icon"
+          theme="subtle"
+          className="absolute right-2"
+          onClick={onClick}
+          icon={
+            <Icon
+              type={type === 'add' ? 'plus' : 'minus'}
+              className={type === 'add' ? 'text-success' : 'text-error'}
+              width={10}
+            />
+          }
+        />
+      </div>
+    );
+  },
+);
 
 export default RivalInput;
