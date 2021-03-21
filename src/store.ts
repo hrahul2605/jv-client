62e662e07101af3d5bf6e0a3340779c07bf4b7f7
hrahul2605 <hrahul2605@gmail.com>
2021-03-21 07:45:46 +0530

    redux dev tools & react hook forms added

--- src/store.ts
+++ src/store.ts
@@ -1,12 +1,16 @@
 import { createStore, applyMiddleware, compose, Action } from 'redux';
 import createSagaMiddleware from 'redux-saga';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import reducers from './reducers';
 import { DefaultRootState } from './reducers/types';
 import sagas from './sagas';
 
 const sagaMiddleware = createSagaMiddleware();
-const enhancers = compose(applyMiddleware(sagaMiddleware));
+const enhancers = compose(
+  applyMiddleware(sagaMiddleware),
+  composeWithDevTools(),
+);
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const store = createStore<DefaultRootState, Action<any>, unknown, unknown>(
