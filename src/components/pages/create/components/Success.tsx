2c60914de67070d878cb0940178c78fa9649ac93
hrahul2605 <hrahul2605@gmail.com>
2021-03-22 13:30:32 +0530

    added timer on success screen to redirect on poll page

--- src/components/pages/create/components/Success.tsx
+++ src/components/pages/create/components/Success.tsx
@@ -1,28 +1,50 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { Redirect, useHistory } from 'react-router-dom';
 import { RESET_NEW_POLL } from '../../../../actions/actionTypes';
 import { useTypedSelector } from '../../../../reducers';
-import { Button, Text } from '../../../atoms';
+import { Button, ProgressBar, Text } from '../../../atoms';
+
+let timeout: null | NodeJS.Timeout = null;
+
+// in seconds
+const REDIRECTION_TIME = 10;
 
 const Success: React.FC = (): React.ReactElement => {
   const { newPoll } = useTypedSelector(state => state.polls);
   const history = useHistory();
   const dispatch = useDispatch();
+
+  const [timer, setTimer] = useState(REDIRECTION_TIME);
   const handleRedirection = () => {
+    if (timeout) {
+      clearTimeout(timeout);
+    }
     history.replace(`/polls/${newPoll?.id}`);
     dispatch({ type: RESET_NEW_POLL });
   };
 
   useEffect(() => {
     if (newPoll?.id) {
-      setTimeout(handleRedirection, 1000);
+      timeout = setInterval(
+        () => setTimer(prev => (prev - 1 < 0 ? 0 : prev - 1)),
+        1000,
+      );
     }
   }, [newPoll?.id]);
 
   return (
     <>
       {!newPoll?.id && <Redirect to="/" />}
+      <div className="absolute mt-24">
+        <ProgressBar
+          loading={timer === 0}
+          onFinish={handleRedirection}
+          label={`You will be redirected in ${timer}s`}
+          interval={100}
+          center
+        />
+      </div>
       <div className="flex flex-col h-full justify-center items-center">
         <Text family="serif" type="display" size="lg" className="mb-12">
           Cnorsgatsluaitons!
