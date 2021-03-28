e6f202b1f58a3248d6e731e09187cff4842c8971
hrahul2605 <hrahul2605@gmail.com>
2021-03-28 13:43:22 +0530

    added toast component molecule

--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -1,2 +1,3 @@
 export { default as RivalInput } from './rivalInput';
 export { default as Choice } from './choice';
+export { default as ToastComponent } from './toastComponent';
---
+++ src/components/molecules/toastComponent/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import toast from 'react-hot-toast';
+import { Button, Icon } from '../../atoms';
+
+interface ToastComponentProps {
+  handleSuccessClick: () => void;
+  text: string | React.ReactNode;
+  id: string;
+}
+
+const ToastComponent: React.FC<ToastComponentProps> = (
+  props,
+): React.ReactElement => {
+  const { handleSuccessClick, text, id } = props;
+  return (
+    <span className="flex flex-row">
+      {text}
+      <div className="flex flex-row ml-2">
+        <Button
+          theme="text"
+          size="s-icon"
+          icon={<Icon type="plus" className="text-white" width={16} />}
+          onClick={handleSuccessClick}
+          className="bg-success border-background"
+        />
+        <Button
+          theme="text"
+          size="s-icon"
+          icon={<Icon type="close" className="text-white" width={16} />}
+          onClick={() => {
+            toast.dismiss(id);
+          }}
+          className="ml-1 bg-error border-background"
+        />
+      </div>
+    </span>
+  );
+};
+
+export default ToastComponent;
