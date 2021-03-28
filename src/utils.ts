2a514b39602d2f2689eb32b9fbba6602bde7613d
hrahul2605 <hrahul2605@gmail.com>
2021-03-28 13:37:53 +0530

    added dayjs lib

--- src/utils.ts
+++ src/utils.ts
@@ -4,3 +4,6 @@ export const getServer = (): string => {
   if (process.env.NODE_ENV !== 'production') return 'http://localhost:5000';
   return process.env.SERVER_URL || 'http://localhost:5000';
 };
+
+export const prettyDateFormat = 'MMMM D, YYYY h:mm A';
+export const dateFormat = 'MM-DD-YYYY HH:mm:ss'; // @TODO: dayjs takes this format only
