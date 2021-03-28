cf1da80c7ed580d14aa5f16bc4be7e1178354f51
hrahul2605 <hrahul2605@gmail.com>
2021-03-28 13:47:55 +0530

    updated polls page to apply start/end time, separted components

--- src/components/pages/polls/index.tsx
+++ src/components/pages/polls/index.tsx
@@ -5,8 +5,8 @@ import { connect } from 'socket.io-client';
 
 import { getPoll } from '../../../api/polls';
 import { useTypedSelector } from '../../../reducers';
-import { Rival } from '../../../reducers/types';
-import { Button, Icon } from '../../atoms';
+import { Poll, Rival } from '../../../reducers/types';
+import { ToastComponent } from '../../molecules';
 import { PollTemplate } from '../../templates';
 
 interface ArgType {
@@ -15,6 +15,7 @@ interface ArgType {
   votes: number;
   googleID: string;
   success: boolean;
+  message: string;
 }
 
 const socket: SocketIOClient.Socket = connect(
@@ -24,49 +25,6 @@ const socket: SocketIOClient.Socket = connect(
     transports: ['websocket'],
   },
 );
-
-interface Poll {
-  googleID: string;
-  title: string;
-  description: string;
-  rivals: Rival[];
-}
-
-interface ToastComponentProps {
-  handleSuccessClick: () => void;
-  title: string;
-  id: string;
-}
-
-const ToastComponent: React.FC<ToastComponentProps> = ({
-  handleSuccessClick,
-  title,
-  id,
-}) => {
-  return (
-    <span className="flex flex-row">
-      Are you sure you want to vote for {title}?
-      <div className="flex flex-row ml-2">
-        <Button
-          theme="text"
-          size="s-icon"
-          icon={<Icon type="plus" className="text-white" width={16} />}
-          onClick={handleSuccessClick}
-          className="bg-success border-background"
-        />
-        <Button
-          theme="text"
-          size="s-icon"
-          icon={<Icon type="close" className="text-white" width={16} />}
-          onClick={() => {
-            toast.dismiss(id);
-          }}
-          className="ml-1 bg-error border-background"
-        />
-      </div>
-    </span>
-  );
-};
 
 const Polls: React.FC = (): React.ReactElement => {
   const { id } = useParams<{ id: string }>();
@@ -87,9 +45,11 @@ const Polls: React.FC = (): React.ReactElement => {
       });
       setPoll({
         description: res.data.description,
-        googleID: res.data.googleID,
+        id: res.data.id,
         title: res.data.title,
         rivals,
+        endTime: res.data.endTime,
+        startTime: res.data.startTime,
       });
       setLoading(false);
     } else {
@@ -103,7 +63,7 @@ const Polls: React.FC = (): React.ReactElement => {
     socket.on('voteUpdate', (args: ArgType) => {
       if (args.success) {
         if (args.googleID === user?.googleID) {
-          toast.success('Your vote has been successfully counted.', {
+          toast.success(args.message, {
             duration: 4000,
           });
           setVoted(args.id);
@@ -126,11 +86,10 @@ const Polls: React.FC = (): React.ReactElement => {
           };
         });
         setTimeout(() => setVoted('-1'), 2000);
-      } else if (args.googleID === user?.googleID) {
-        toast.error('Only 1 vote per user allowed.', {
+      } else if (args.googleID === user?.googleID)
+        toast.error(args.message, {
           duration: 4000,
         });
-      }
     });
     return () => {
       socket.removeAllListeners();
@@ -155,7 +114,7 @@ const Polls: React.FC = (): React.ReactElement => {
                 100,
               );
             }}
-            title={title}
+            text={`Are you sure you want to vote for ${title}?`}
           />
         ),
         {
