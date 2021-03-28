966246c8e495d9de6cad7340a1e2d17f76c707a9
hrahul2605 <hrahul2605@gmail.com>
2021-03-28 21:17:50 +0530

    robots.txt, upd getServer
    Signed-off-by: hrahul2605 <hrahul2605@gmail.com>

--- src/utils.ts
+++ src/utils.ts
@@ -3,7 +3,6 @@ import { Dayjs } from 'dayjs';
 export const tuple = <T extends string[]>(...args: T): [...typeof args] => args;
 
 export const getServer = (): string => {
-  if (process.env.NODE_ENV !== 'production') return 'http://localhost:5000';
   return process.env.SERVER_URL || 'http://localhost:5000';
 };
 
